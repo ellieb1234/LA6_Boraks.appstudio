0d91b5343bb97e1110b9d9af120734d514c2f9f8
ellieb1234 <ellieboraks@creighton.edu>
2021-03-16 21:22:45 -0500

    membership

--- LA6Boraks.appstudio/forms/membership/membership.js
+++ LA6Boraks.appstudio/forms/membership/membership.js
@@ -1,5 +1,13 @@
 
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
-Button1.onclick=function(){
+btnSubmit.onclick=function(){
+  for(i=0;i<members.length;i++){
+    if(pitName.value == members[i])
+      lblNotify.value='You are a member.'
+  }
+  if(lblNotify.value!='You are a member.'){
+    lblNotify.value='You are not a member.'
+    members.push(iptName.value)
+  }
 }
